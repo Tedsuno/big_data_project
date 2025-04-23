3db0d0cae0f352c7960f8265b38527f9c04f1f85
Tedsuno <121364946+Tedsuno@users.noreply.github.com>
2025-04-23 21:13:38 +0200

    Update train_lstm.py

--- src/solutions/lstm_classifier/train_lstm.py
+++ src/solutions/lstm_classifier/train_lstm.py
@@ -6,6 +6,7 @@ from tensorflow.keras.layers import Embedding, SpatialDropout1D, LSTM, Dense
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 from model_lstm import create_lstm_model
+from plot import plot_training_history, analyze_misclassified
 
 def print_results(validation_size, X_train, X_test, Y_train, Y_test, tokenizer,model) :
     if len(X_test) <= validation_size:
@@ -51,28 +52,37 @@ def print_results(validation_size, X_train, X_test, Y_train, Y_test, tokenizer,m
 def main():
     batch_size = 32
     max_features = 5000
-    
-    # Chargement et prétraitement des données DE MONGODB 
-    print(" Chargement et prétraitement des données DE MONGODB et création du modèle LSTM...")
-    
-    # Creation of the model LSTM
-    X_train, X_test, Y_train, Y_test, tokenizer = preprocess_data(28, 5000)
-    model = create_lstm_model(max_features, X_train.shape[1],Y_train,X_train)
-    
-    #Entrainement du modele LSTM
+    max_len = 28
+
+    print("Chargement et prétraitement UNIQUE des données et création du modèle LSTM...")
+
+    X_train, X_test, Y_train, Y_test, texts_train, texts_test, tokenizer = preprocess_data(
+        max_len=max_len,
+        vocab_size=max_features,
+        test_size=0.33,
+        random_state=42
+    )
+    if X_train is None:
+         print("Le prétraitement des données a échoué. Arrêt.")
+         return
+
+    # Création du modèle LSTM
+    model = create_lstm_model(max_features, X_train.shape[1], Y_train, X_train)
+    #Entrainement du modele
     print("Entrainement")
-    model.fit(X_train, Y_train, epochs=10, batch_size=batch_size, verbose=2, validation_split=0.1)
-    
-    #Evaluation du modele LSTM
+    history = model.fit(X_train, Y_train, epochs=10, batch_size=batch_size, verbose=2, validation_split=0.1)
+    #Evaluation de l'accuracy
     print("Evaluation")
     score, acc = model.evaluate(X_test, Y_test, verbose=2, batch_size=batch_size)
-    print("Score: %.2f" % score)
-    print("Accuracy: %.2f" % acc)
-    
-    #Test de validation final
+    print(f"Score: {score:.2f}")
+    print(f"Accuracy: {acc:.2f}")
+    #Affichage des résulats numériques
     validation_size = 1500
-    print_results(validation_size, X_train, X_test, Y_train, Y_test, tokenizer,model)
-    
+    print_results(validation_size, X_train, X_test, Y_train, Y_test, tokenizer, model)
+
+    # Affichage des graphiques
+    plot_training_history(history) 
+    analyze_misclassified(model, X_test, Y_test, texts_test, tokenizer) 
 
 
 if __name__ == "__main__":
